0e1d94c3b9e75c32fae183710cc963b4f6253948
David Airlambang <david.airlambang@gmail.com>
2023-05-23 21:02:24 +0700

    Connect to Firestore and create user

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const admin = require('firebase-admin');
-const credentials = require('./serviceAccountKey.json');
-const userRouter = require('./routes/userRoutes');
-const globalErrorHandler = require('./controllers/errorController');
-const AppError = require('./utils/appError');
+const express = require('express')
+const admin = require('firebase-admin')
+const credentials = require('./serviceAccountKey.json')
+const userRouter = require('./routes/userRoutes')
+const globalErrorHandler = require('./controllers/errorController')
+const AppError = require('./utils/appError')
 
-const app = express();
+const app = express()
 
 admin.initializeApp({
-  credential: admin.credential.cert(credentials),
-});
+  credential: admin.credential.cert(credentials)
+})
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 // ROUTES
-app.use('/users', userRouter);
+app.use('/users', userRouter)
 
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-});
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
+})
 
-app.use(globalErrorHandler);
+app.use(globalErrorHandler)
 
-module.exports = app;
+module.exports = app
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,49 +1,61 @@
-const admin = require('firebase-admin');
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
+const admin = require('firebase-admin')
+const catchAsync = require('../utils/catchAsync')
+const AppError = require('../utils/appError')
 
 exports.register = catchAsync(async (req, res, next) => {
   // Error handling
   if (!req.body.email || !req.body.password)
-    return next(new AppError('Please provide email and password!', 400));
+    return next(new AppError('Please provide email and password!', 400))
 
+  //firebase
   const newUser = await admin.auth().createUser({
     email: req.body.email,
     password: req.body.password,
     emailVerified: false,
-    disabled: false,
-  });
+    disabled: false
+  })
 
+  console.log(newUser.uid)
+  //firestore
+  const db = admin.firestore()
+  const uid = newUser.uid
+  const userJson = {
+    email: newUser.email,
+    uid: uid
+  }
+  const response = await db.collection('users').doc(uid).set(userJson)
+
+  //success
   res.status(201).json({
     status: 'success',
     data: {
-      user: newUser,
-    },
-  });
-});
+      user: newUser
+    }
+  })
+})
 
 exports.deleteByEmail = catchAsync(async (req, res, next) => {
   // Error handling
-  if (!req.body.email) return next(new AppError('Please provide email!', 400));
+  if (!req.body.email) return next(new AppError('Please provide email!', 400))
 
-  const user = await admin.auth().getUserByEmail(req.body.email);
+  const user = await admin.auth().getUserByEmail(req.body.email)
 
-  await admin.auth().deleteUser(user.uid);
+  await admin.auth().deleteUser(user.uid)
 
   res.status(204).json({
     status: 'success',
-    data: null,
-  });
-});
+    data: null
+  })
+})
 
 exports.deleteByUid = catchAsync(async (req, res, next) => {
   // Error handling
-  if (!req.body.uid) return next(new AppError('Please provide uid!', 400));
+  if (!req.body.uid) return next(new AppError('Please provide uid!', 400))
 
-  await admin.auth().deleteUser(req.body.uid);
+  await admin.auth().deleteUser(req.body.uid)
 
   res.status(204).json({
     status: 'success',
-    data: null,
-  });
-});
+    data: null
+  })
+})
